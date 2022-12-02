1e10123f0162e89f68fc8edec6d59ea7bae03587
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-12-02 10:58:17 -0300

    feat: criando projeto

---
+++ first-project/index.js
@@ -0,0 +1,10 @@
+import "@bacons/expo-metro-runtime";
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
+
+export function App(){
+    const ctx = require.context("./app");
+    return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
