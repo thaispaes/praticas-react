82f8bb7f62cbef4cd1e234f191387252e26c818e
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-12-02 10:06:02 -0300

    feat: importando router

--- first-project/App.js
+++ first-project/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+    return (
+      <>
+      
+      </>
+    );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
