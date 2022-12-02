d0add68ed05b7735173353a804bd16a20e0c062d
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-12-02 12:22:47 -0300

    feat: esqueleto da image page

--- first-project/app/photo.js
+++ first-project/app/photo.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button } from "react-native";
 export default function Page({ navigation }) {
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Pagina da Imagem</Text>
+        <Text style={styles.title}>ASSISTA DEMON SLAYER</Text>
 
         <Button onPress={() => navigation.goBack()} title="Voltar"/>
     </View>
@@ -14,14 +14,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    textAlign: "center",
+    backgroundColor: "#652d87",
     justifyContent: "center"
   },
   title: {
     fontSize: 64,
+    color: "#FFFFFF",
     fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+    paddingBottom: "50px"
+  }
 });
